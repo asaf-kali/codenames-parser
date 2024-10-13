7df9bf5d9b6d509bfe23335b34df0ec850dcb6b6
Asaf Kali <asaf.kali@mail.huji.ac.il>
2024-10-13 20:16:40 +0300

    Auto commit.

--- codenames_parser/align.py
+++ codenames_parser/align.py
@@ -16,15 +16,17 @@ def align_image(image: np.ndarray) -> np.ndarray:
         result = _align_image_iteration(image, rho=rho)
         if result.line_count < 10:
             break
-        image = result.image
+        if abs(result.angle_degrees) < 0.01:
+            break
+        image = result.aligned_image
         rho /= 1.5
-    save_debug_image(image, title="aligned_final")
     return image
 
 
 class AlignmentIterationResult(NamedTuple):
-    image: np.ndarray
+    aligned_image: np.ndarray
     line_count: int
+    angle_degrees: float
 
 
 def _align_image_iteration(image: np.ndarray, rho: float) -> AlignmentIterationResult:
@@ -32,13 +34,13 @@ def _align_image_iteration(image: np.ndarray, rho: float) -> AlignmentIterationR
     edges = detect_edges(blurred)
     lines = extract_lines(edges, rho=rho)
     if not lines:
-        return AlignmentIterationResult(image, 0)
+        return AlignmentIterationResult(image, 0, 0)
     draw_lines(blurred, lines=lines, title="lines_before_rotate")
     angle_degrees = _find_rotation_angle(lines)
     log.info(f"Rotation angle: {angle_degrees}")
     aligned_image = _rotate_by(image, angle_degrees)
     save_debug_image(aligned_image, title="aligned")
-    return AlignmentIterationResult(aligned_image, len(lines))
+    return AlignmentIterationResult(aligned_image=aligned_image, line_count=len(lines), angle_degrees=angle_degrees)
 
 
 def _rotate_by(image: np.ndarray, angle_degrees: float) -> np.ndarray:
@@ -64,30 +66,40 @@ def _find_rotation_angle(lines: list[Line]) -> float:
     grid_lines = _get_grid_lines(lines)
     sum = count = 0
     if grid_lines.horizontal:
-        sum += np.mean([abs(line.theta - np.pi / 2) for line in grid_lines.horizontal])
+        horizontal_diff = np.mean([_horizontal_diff(line.theta) for line in grid_lines.horizontal])
+        sum += horizontal_diff
         count += 1
     if grid_lines.vertical:
-        sum += np.mean([min(line.theta, np.pi - line.theta) for line in grid_lines.vertical])
+        vertical_diff = np.mean([_vertical_diff(line.theta) for line in grid_lines.vertical])
+        sum += vertical_diff
         count += 1
     if count == 0:
         return 0
     angle = sum / count
-    angle_degrees = -np.degrees(angle)
+    angle_degrees = np.degrees(angle)
     return angle_degrees
+
+
+def _horizontal_diff(theta: float) -> float:
+    return float(np.mod(theta + np.pi / 2, np.pi) - np.pi / 2)
+
+
+def _vertical_diff(theta: float) -> float:
+    return float(theta - np.pi / 2)
 
 
 def _get_grid_lines(lines: list[Line]) -> GridLines:
     horizontal = []
     vertical = []
     for line in lines:
-        if _is_vertical_line(line):
-            vertical.append(line)
-        else:
+        if _is_horizontal_line(line):
             horizontal.append(line)
-    return GridLines(horizontal, vertical)
+        else:
+            vertical.append(line)
+    return GridLines(horizontal=horizontal, vertical=vertical)
 
 
-def _is_vertical_line(line: Line) -> bool:
+def _is_horizontal_line(line: Line) -> bool:
     if line.theta < np.pi / 4 or line.theta > 3 * np.pi / 4:
         return True
     return False
