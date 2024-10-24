a9557c2e7a451c0ce0eba9d07950c7159bcdb6e7
Asaf Kali <asaf.kali@mail.huji.ac.il>
2024-10-25 01:04:20 +0300

    Add board test cases

--- tests/b_board/cases.py
+++ tests/b_board/cases.py
@@ -5,68 +5,134 @@ class ParseBoardTestCase(NamedTuple):
     fixture_file: str
     language: str
     expected_words: list[str]
+    allowed_errors: int = 1
 
+
+BOARD_1_WORDS = [
+    "קוסם",
+    "פסל",
+    "דיונון",
+    "עגיל",
+    "קבוצה",
+    "קרחון",
+    "נמלה",
+    "שקוף",
+    "מכשף",
+    "עורב",
+    "נפח",
+    "צמח",
+    "ברזל",
+    "מערה",
+    "טרול",
+    "זומבי",
+    "פרשן",
+    "נביא",
+    "פנינה",
+    "טבע",
+    "אבק",
+    "מיקרופון",
+    "שידור",
+    "גביע",
+    "פרס",
+]
+
+BOARD_2_WORDS = [
+    "סתיו",
+    "קרב",
+    "אביב",
+    "עגיל",
+    "קבוצה",
+    "מקרה",
+    "ביקור",
+    "צעצוע",
+    "שיתוף",
+    "יין",
+    "שקר",
+    "במה",
+    "אהדה",
+    "חברה",
+    "נצח",
+    "צריח",
+    "טיול",
+    "יציאה",
+    "פנינה",
+    "טבע",
+    "סוחר",
+    "זאב",
+    "צידה",
+    "גביע",
+    "כותונת",
+]
+
+BOARD_3_WORDS = [
+    "ציבור",
+    "אוטובוס",
+    "ישראל",
+    "מתח",
+    "גס",
+    "ברית",
+    "גוש",
+    "איום",
+    "מורח",
+    "קנה",
+    "לידה",
+    "מבחן",
+    "אודם",
+    "שוקו",
+    "חטיף",
+    "חוק",
+    "רץ",
+    "חצות",
+    "רדיו",
+    "כתם",
+    "גרם",
+    "כהן",
+    "רושם",
+    "אלמוג",
+    "אופק",
+]
+
+BOARD1_CASE = ParseBoardTestCase(
+    fixture_file="board1.jpg",
+    language="heb",
+    expected_words=BOARD_1_WORDS,
+)
 
 BOARD1_TOP_CASE = ParseBoardTestCase(
     fixture_file="board1_top.jpg",
     language="heb",
-    expected_words=[
-        "קוסם",
-        "פסל",
-        "דיונון",
-        "עגיל",
-        "קבוצה",
-        "קרחון",
-        "נמלה",
-        "שקוף",
-        "מכשף",
-        "עורב",
-        "נפח",
-        "צמח",
-        "ברזל",
-        "מערה",
-        "טרול",
-        "זומבי",
-        "פרשן",
-        "נביא",
-        "פנינה",
-        "טבע",
-        "אבק",
-        "מיקרופון",
-        "שידור",
-        "גביע",
-        "פרס",
-    ],
+    expected_words=BOARD_1_WORDS,
+)
+
+BOARD2_CASE = ParseBoardTestCase(
+    fixture_file="board2.jpg",
+    language="heb",
+    expected_words=BOARD_2_WORDS,
+    allowed_errors=4,
+)
+
+BOARD2_TOP_CASE = ParseBoardTestCase(
+    fixture_file="board2_top.jpg",
+    language="heb",
+    expected_words=BOARD_2_WORDS,
+    allowed_errors=5,
+)
+
+BOARD3_TILT_CASE = ParseBoardTestCase(
+    fixture_file="board3_tilt.jpg",
+    language="heb",
+    expected_words=BOARD_3_WORDS,
 )
 
 BOARD3_TOP_CASE = ParseBoardTestCase(
     fixture_file="board3_top.jpg",
     language="heb",
-    expected_words=[
-        "ציבור",
-        "אוטובוס",
-        "ישראל",
-        "מתח",
-        "גס",
-        "ברית",
-        "גוש",
-        "איום",
-        "מורח",
-        "קנה",
-        "לידה",
-        "מבחן",
-        "אודם",
-        "שוקו",
-        "חטיף",
-        "חוק",
-        "רץ",
-        "חצות",
-        "רדיו",
-        "כתם",
-        "גרם",
-        "כהן",
-        "רושם",
-        "אלמוג",
-        "אופק",
-    ],
+    expected_words=BOARD_3_WORDS,
+)
+
+BOARD3_TOP2_CASE = ParseBoardTestCase(
+    fixture_file="board3_top2.jpg",
+    language="heb",
+    expected_words=BOARD_3_WORDS,
+    allowed_errors=3,
 )
--- tests/b_board/test_board_parsing.py
+++ tests/b_board/test_board_parsing.py
@@ -1,12 +1,22 @@
 import logging
 
+import pytest
 from codenames.game.board import Board
 from codenames.game.card import Card
 
 from codenames_parser.board.board_parser import parse_board
 from codenames_parser.common.grid_detection import GRID_WIDTH
 from codenames_parser.common.models import Grid
-from tests.b_board.cases import BOARD1_TOP_CASE, BOARD3_TOP_CASE, ParseBoardTestCase
+from tests.b_board.cases import (
+    BOARD1_CASE,
+    BOARD1_TOP_CASE,
+    BOARD2_CASE,
+    BOARD2_TOP_CASE,
+    BOARD3_TILT_CASE,
+    BOARD3_TOP2_CASE,
+    BOARD3_TOP_CASE,
+    ParseBoardTestCase,
+)
 from tests.fixtures import get_fixture_path
 from tests.utils import print_diff
 
@@ -23,12 +33,33 @@ def _test_parse_board(case: ParseBoardTestCase):
     actual_grid = Grid.from_list(row_size=GRID_WIDTH, items=words)
     diff = expected_grid.diff(other=actual_grid)
     print_diff(diff)
-    assert len(diff) <= 1
+    assert len(diff) <= case.allowed_errors
+
+
+def test_parse_board_case_1():
+    _test_parse_board(BOARD1_CASE)
 
 
 def test_parse_board_case_1_top():
     _test_parse_board(BOARD1_TOP_CASE)
 
 
+def test_parse_board_case_2():
+    _test_parse_board(BOARD2_CASE)
+
+
+def test_parse_board_case_2_top():
+    _test_parse_board(BOARD2_TOP_CASE)
+
+
+@pytest.mark.skip("Tilt is not yet supported")
+def test_parse_board_case_3_tilt():
+    _test_parse_board(BOARD3_TILT_CASE)
+
+
 def test_parse_board_case_3_top():
     _test_parse_board(BOARD3_TOP_CASE)
+
+
+def test_parse_board_case_3_top2():
+    _test_parse_board(BOARD3_TOP2_CASE)
