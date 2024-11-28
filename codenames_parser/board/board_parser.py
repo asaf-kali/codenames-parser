🐛 Box scaling fixes

codenames_parser/board/board_parser.py
@@ -6,6 +6,7 @@ from codenames_parser.board.card_parser import parse_cards
 from codenames_parser.board.grid_detection import extract_boxes
 from codenames_parser.common.impr.align import align_image, apply_rotations
 from codenames_parser.common.impr.crop import crop_by_box
+from codenames_parser.common.impr.equalization import contrast_limit_equalization
 from codenames_parser.common.impr.scale import scale_down_image
 from codenames_parser.common.utils.debug_util import draw_boxes
 from codenames_parser.common.utils.models import Box
@@ -15,26 +16,22 @@ def parse_board(image: np.ndarray, language: str) -> list[str]:
     scale_result = scale_down_image(image)
     alignment_result = align_image(scale_result.image)
     boxes = extract_boxes(image=alignment_result.aligned_image)
+    equalized = contrast_limit_equalization(image)
     cells = _crop_cells(
-        image=image,
+        image=equalized,
         boxes=boxes,
         scale_factor=1 / scale_result.scale_factor,
         rotations=alignment_result.rotations,
-        enlarge_factor=0.25,
     )
     cards = parse_cards(cells, language=language)
     return cards
 
 
-def _crop_cells(
-    image: np.ndarray, boxes: list[Box], scale_factor: float, rotations: list[float], enlarge_factor: float
-) -> list[np.ndarray]:
+def _crop_cells(image: np.ndarray, boxes: list[Box], scale_factor: float, rotations: list[float]) -> list[np.ndarray]:
     image_rotated = apply_rotations(image, rotations=rotations)
-    boxes_scaled = [_scale_box(box, scale_factor) for box in boxes]
+    boxes_scaled = [_scale_box(box, scale_factor=scale_factor) for box in boxes]
     draw_boxes(image_rotated, boxes=boxes_scaled, title="boxes scaled", thickness=5)
-    boxes_enlarged = [_box_enlarged(box, factor=enlarge_factor) for box in boxes_scaled]
-    draw_boxes(image_rotated, boxes=boxes_enlarged, title="boxes enlarged", thickness=5)
-    cells = [crop_by_box(image_rotated, box=box) for box in boxes_enlarged]
+    cells = [crop_by_box(image_rotated, box=box) for box in boxes_scaled]
     return cells
 
 

codenames_parser/board/grid_detection.py
@@ -25,7 +25,7 @@ log = logging.getLogger(__name__)
 
 WHITE = Color(255, 255, 255)
 CARD_RATIO = 1.55
-UNCERTAIN_BOX_FACTOR = 1.1
+UNCERTAIN_BOX_FACTOR = 1.25
 COLOR_MASK_PERCENTILES = [80, 70, 60, 50]
 
 
@@ -85,7 +85,7 @@ def detect_card_boxes(image: np.ndarray, negative: np.ndarray, percentile: int) 
     return card_boxes
 
 
-def _complete_missing_boxes(boxes: list[Box]) -> list[Box]:
+def _complete_missing_boxes(boxes: list[Box], uncertain_box_factor: float = UNCERTAIN_BOX_FACTOR) -> list[Box]:
     """
     Complete missing boxes in the list to reach the expected GRID_SIZE.
     Boxes might not be exactly aligned in a grid, so we can't assume constant row and column sizes.
@@ -130,8 +130,8 @@ def _complete_missing_boxes(boxes: list[Box]) -> list[Box]:
 
     # Map the assignments
     grid_positions = _predict_missing_boxes_centers(assigned_boxes=assigned_boxes, grid_positions=grid_positions)
-    width_uncertain = average_width * UNCERTAIN_BOX_FACTOR
-    height_uncertain = average_height * UNCERTAIN_BOX_FACTOR
+    width_uncertain = average_width * uncertain_box_factor
+    height_uncertain = average_height * uncertain_box_factor
     width_offset = (width_uncertain - average_width) / 2
     height_offset = (height_uncertain - average_height) / 2
 